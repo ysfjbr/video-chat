e8b77d781a5d8976e846295e582da2ce41d7138a
ysfjbr <ysf_jbr@msn.com>
2021-03-20 21:16:22 +0100

    imp data channel

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -1,5 +1,75 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import io from 'socket.io-client'
+import styled from "styled-components";
+ 
+
+const Container = styled.div`
+    height: 100vh;
+    width: 50%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Messages = styled.div`
+    width: 100%;
+    height: 60%;
+    border: 1px solid black;
+    margin-top: 10px;
+    overflow: scroll;
+`;
+
+const MessageBox = styled.textarea`
+    width: 100%;
+    height: 30%;
+`;
+
+const Button = styled.div`
+    width: 50%;
+    border: 1px solid black;
+    margin-top: 15px;
+    height: 5%;
+    border-radius: 5px;
+    cursor: pointer;
+    background-color: black;
+    color: white;
+    font-size: 18px;
+`;
+
+const MyRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
+`;
+
+const MyMessage = styled.div`
+  width: 45%;
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  margin-right: 5px;
+  text-align: center;
+  border-top-right-radius: 10%;
+  border-bottom-right-radius: 10%;
+`;
+
+const PartnerRow = styled(MyRow)`
+  justify-content: flex-start;
+`;
+
+const PartnerMessage = styled.div`
+  width: 45%;
+  background-color: grey;
+  color: white;
+  border: 1px solid lightgray;
+  padding: 10px;
+  margin-left: 5px;
+  text-align: center;
+  border-top-left-radius: 10%;
+  border-bottom-left-radius: 10%;
+`;
 
 const Room = (props) => {
     
@@ -9,6 +79,9 @@ const Room = (props) => {
     const socketRef = useRef()
     const otherUser = useRef()
     const userStream = useRef()
+    const sendChannel = useRef()
+    const [text, setText] = useState("");
+    const [messages, setMessages] = useState([])
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
@@ -39,6 +112,8 @@ const Room = (props) => {
     function callUser(userID) {
         peerRef.current = createPeer(userID);
         userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
+        sendChannel.current = peerRef.current.createDataChannel('sendChannel')
+        sendChannel.current.onmessage = handleRecieveMessage
     }
 
     function createPeer(userID) {
@@ -62,6 +137,19 @@ const Room = (props) => {
         return peer;
     }
 
+    function handleRecieveMessage(e){
+        setMessages(messages => [ ...messages, { yours: false, value: e.data}])
+    }
+
+    function sendMessage(){
+        sendChannel.current.send(text)
+        setMessages(messages => [ ...messages, { yours: true, value: text}])
+    }
+
+    function handleChange(e) {
+        setText(e.target.value);
+    }
+
     function handleNegotiationNeededEvent(userID){
         peerRef.current.createOffer().then(offer => {
             return peerRef.current.setLocalDescription(offer)
@@ -77,6 +165,11 @@ const Room = (props) => {
 
     function handleRecieveCall(incoming){
         peerRef.current = createPeer()
+        peerRef.current.ondatachannel = (e) => {
+            sendChannel.current = e.channel
+            sendChannel.current.onmessage = handleRecieveMessage
+        }
+
         const desc = new RTCSessionDescription(incoming.sdp)
         peerRef.current.setRemoteDescription(desc)
         .then(()=> {
@@ -121,10 +214,36 @@ const Room = (props) => {
         partnerVideo.current.srcObject = e.streams[0]
     }
 
+    function renderMessage(message, index) {
+        if (message.yours) {
+            return (
+                <MyRow key={index}>
+                    <MyMessage>
+                        {message.value}
+                    </MyMessage>
+                </MyRow>
+            )
+        }
+
+        return (
+            <PartnerRow key={index}>
+                <PartnerMessage>
+                    {message.value}
+                </PartnerMessage>
+            </PartnerRow>
+        )
+    }
+
     return (
         <div>
             <video autoPlay ref={userVideo} />
             <video autoPlay ref={partnerVideo} />
+
+            <Messages>
+                {messages.map(renderMessage)}
+            </Messages>
+            <MessageBox value={text} onChange={handleChange} placeholder="Say something....." />
+            <Button onClick={sendMessage}>Send..</Button>
         </div>
     )
 }
